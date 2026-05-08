f36d7abb3ad8c1ca00a03f52dc5b7fbcdd5eccae
Majid-Shahani <majid.shahani.1390@gmail.com>
2026-05-08 19:12:26 +0300

    Convert stations to classes

--- src/station.js
+++ src/station.js
@@ -16,144 +16,151 @@ const canAction = (ing) => {
 };
 
 // action is CUT / CRUSH
-function Station(action) {
-  this.inventory = 0;
-  this.canMoveWhileWorking = false;
+class Station {
+  constructor(action) {
+    this.inventory = 0;
+    this.canMoveWhileWorking = false;
 
-  this.canWork = true;
-  this.doWork = () => {
-    if (this.canWork) {
-      this.canWork = false;
-      // wait timer
-      this.inventory <<= action;
-    }
-  };
+    this.canWork = true;
+    this.doWork = () => {
+      if (this.canWork) {
+        this.canWork = false;
+        // wait timer
+        this.inventory <<= action;
+      }
+    };
 
-  this.canPlace = (playerInv) =>
-    playerInv.ingredient < ingredients.CUTOFF &&
-    !playerInv.hasGlass() &&
-    this.inventory === 0 &&
-    this.canAction(playerInv.ingredient);
+    this.canPlace = (playerInv) =>
+      playerInv.ingredient < ingredients.CUTOFF &&
+      !playerInv.hasGlass() &&
+      this.inventory === 0 &&
+      this.canAction(playerInv.ingredient);
 
-  this.place = (playerInv) => {
-    if (this.canPlace(playerInv)) {
-      this.inventory = playerInv.ingredient;
-      playerInv.ingredient = 0;
-      return true;
-    }
-    return false;
-  };
-
-  this.canTake = (playerInv) =>
-    playerInv.ingredient === 0 && !playerInv.hasGlass();
-  this.take = (playerInv) => {
-    if (this.canTake(playerInv)) {
-      this.canWork = true;
-      playerInv.ingredient = this.inventory;
-      this.inventory = 0;
-      return true;
-    }
-    return false;
-  };
-}
-
-const Ingredient = (ing) => {
-  return {
-    inventory: ing,
-
-    canWork: () => false,
-    canPlace: () => false,
-
-    canTake: (playerInv) => playerInv.empty(),
-    take: (playerInv) => {
-      if (canTake(playerInv)) {
-        playerInv.ingredient = inventory;
+    this.place = (playerInv) => {
+      if (this.canPlace(playerInv)) {
+        this.inventory = playerInv.ingredient;
+        playerInv.ingredient = 0;
         return true;
       }
       return false;
-    },
-  };
-};
+    };
 
-const Glass = (glassType) => {
-  return {
-    inventory: glassType,
+    this.canTake = (playerInv) =>
+      playerInv.ingredient === 0 && !playerInv.hasGlass();
+    this.take = (playerInv) => {
+      if (this.canTake(playerInv)) {
+        this.canWork = true;
+        playerInv.ingredient = this.inventory;
+        this.inventory = 0;
+        return true;
+      }
+      return false;
+    };
+  }
+}
 
-    canWork: () => false,
-    canPlace: () => false,
+class Ingredient {
+  constructor(ing) {
+    this.inventory = ing;
 
-    canTake: (playerInv) => playerInv.empty(),
-    take: (playerInv) => {
-      if (canTake(playerInv)) {
-        playerInv.glass.type = inventory;
+    this.canWork = () => false;
+    this.canPlace = () => false;
+
+    this.canTake = (playerInv) => playerInv.empty();
+    this.take = (playerInv) => {
+      if (this.canTake(playerInv)) {
+        playerInv.ingredient = this.inventory;
+        return true;
+      }
+      return false;
+    };
+  }
+}
+
+class Glass {
+  constructor(glassType) {
+    this.inventory = glassType;
+
+    this.canWork = () => false;
+    this.canPlace = () => false;
+
+    this.canTake = (playerInv) => playerInv.empty();
+    this.take = (playerInv) => {
+      if (this.canTake(playerInv)) {
+        playerInv.glass.type = this.inventory;
         playerInv.glass.inventory = 0;
         return true;
       }
       return false;
-    },
-  };
-};
+    };
+  }
+}
 
-const cauldron = {
-  inventory: 0,
-  canMoveWhileWorking: true,
-  fire: false,
+class Cauldron {
+  constructor() {
+    this.inventory = 0;
+    this.canMoveWhileWorking = true;
+    this.fire = false;
 
-  canPlace: (playerInv) => !playerInv.hasGlass() && playerInv.ingredient !== 0,
-  place: (playerInv) => {
-    if (canPlace(playerInv)) {
-      inventory |= playerInv.ingredient;
-      playerInv.ingredient = 0;
-      return true;
-    }
-    return false;
-  },
-
-  canTake: (playerInv) =>
-    playerInv.ingredient === 0 &&
-    playerInv.hasGlass() &&
-    playerInv.glass.inventory === 0,
-  take: (playerInv) => {
-    if (canTake(playerInv)) {
-      playerInv.glass.inventory = inventory;
-      inventory = 0;
-      return true;
-    }
-  },
-
-  canWork: true,
-  doWork: () => {
-    if (fire) fire = false;
-    else fire = true;
-  },
-
-  // must run on timer while fire = true
-  brew: () => {
-    let new_inventory = 0;
-    for (let i = 0; i < getIndex(ingredients.CUTOFF); i++) {
-      if ((inventory & (1 << i)) !== 0) {
-        const brewed = brew_map[i];
-        if (brewed !== undefined) new_inventory |= brewed;
-        else new_inventory |= ingredients.Overbrewed;
+    this.canPlace = (playerInv) =>
+      !playerInv.hasGlass() && playerInv.ingredient !== 0;
+    this.place = (playerInv) => {
+      if (this.canPlace(playerInv)) {
+        this.inventory |= playerInv.ingredient;
+        playerInv.ingredient = 0;
+        return true;
       }
-    }
-    inventory = new_inventory;
-  },
-};
+      return false;
+    };
+
+    this.canTake = (playerInv) =>
+      playerInv.ingredient === 0 &&
+      playerInv.hasGlass() &&
+      playerInv.glass.inventory === 0;
+    this.take = (playerInv) => {
+      if (this.canTake(playerInv)) {
+        playerInv.glass.inventory = this.inventory;
+        this.inventory = 0;
+        return true;
+      }
+    };
+
+    this.canWork = true;
+    this.doWork = () => {
+      if (this.fire) this.fire = false;
+      else this.fire = true;
+    };
+
+    // must run on timer while fire = true
+    this.brew = () => {
+      let new_inventory = 0;
+      for (let i = 0; i < getIndex(ingredients.CUTOFF); i++) {
+        if ((this.inventory & (1 << i)) !== 0) {
+          const brewed = brew_map[i];
+          if (brewed !== undefined) new_inventory |= brewed;
+          else new_inventory |= ingredients.Overbrewed;
+        }
+      }
+      this.inventory = new_inventory;
+    };
+  }
+}
+
+const cauldron = new Cauldron();
 
 const cutStation = new Station(CUT);
 const crushStation = new Station(CRUSH);
 
-const roundGlassBox = Glass("ROUND");
-const squareGlassBox = Glass("SQUARE");
+const roundGlassBox = new Glass("ROUND");
+const squareGlassBox = new Glass("SQUARE");
 
-const petalBox = Ingredient(ingredients.AsphodelPetals);
-const scaleBox = Ingredient(ingredients.DragonScales);
-const mandrakeBox = Ingredient(ingredients.Mandrake);
-const figBox = Ingredient(ingredients.Shrivelfig);
-const vinumVase = Ingredient(ingredients.Vinum);
-const olemVase = Ingredient(ingredients.Olem);
-const aquaVase = Ingredient(ingredients.Aqua);
+const petalBox = new Ingredient(ingredients.AsphodelPetals);
+const scaleBox = new Ingredient(ingredients.DragonScales);
+const mandrakeBox = new Ingredient(ingredients.Mandrake);
+const figBox = new Ingredient(ingredients.Shrivelfig);
+const vinumVase = new Ingredient(ingredients.Vinum);
+const olemVase = new Ingredient(ingredients.Olem);
+const aquaVase = new Ingredient(ingredients.Aqua);
 
 export {
   cauldron,
