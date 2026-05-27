c0f07143247d1c619d42578ed1c0d227392e23cf
Majid-Shahani <majid.shahani.1390@gmail.com>
2026-05-27 18:07:14 +0300

    Add heat observer

--- src/data/gameData.js
+++ src/data/gameData.js
@@ -3,20 +3,40 @@ const gameData = {
   heat: 1,
 };
 
+let heatListeners = [];
+function attachHeatListener(observer) {
+  if (heatListeners.includes(observer)) return false;
+
+  heatListeners.push(observer);
+  return true;
+}
+function detachHeatListener(observer) {
+  const index = heatListeners.indexOf(observer);
+  if (index === -1) return false;
+
+  heatListeners.splice(index, 1);
+  return true;
+}
+
+function callHeatListeners() {
+  heatListeners.forEach((observer) => observer(gameData.heat));
+}
+
+function increaseHeat() {
+  gameData.heat++;
+  callHeatListeners();
+}
+
 const activeOrders = [];
 function makeDelivery() {
   gameData.essence += 25;
 
   currentState.ordersFullfilled++;
   if (currentState.ordersFullfilled >= currentState.nextHeat) {
-    // increase heat
-    gameData.heat++;
-    currentState.nextHeat += gameData.heat + 1;
-    currentState.baseTime = defaultState.baseTime / (1 + gameData.heat);
-    currentState.timeToNextOrder =
-      defaultState.orderAdditionRatio * currentState.baseTime;
+    increaseHeat();
   }
 }
+
 function addOrder(recipe) {
   if (activeOrders.length > 2) return;
   activeOrders.push({
@@ -45,11 +65,16 @@ const currentState = {
 function resetState() {
   gameData.essence = 0;
   gameData.heat = 1;
-
-  currentState.baseTime = defaultState.baseTime / (1 + gameData.heat);
-  currentState.timeToNextOrder =
-    defaultState.orderAdditionRatio * defaultState.baseTime;
   currentState.ordersFullfilled = 0;
+
+  heatListeners.length = 0;
+  attachHeatListener((heatLevel) => {
+    currentState.nextHeat += heatLevel + 1;
+    currentState.baseTime = defaultState.baseTime / (1 + heatLevel);
+    currentState.timeToNextOrder =
+      defaultState.orderAdditionRatio * currentState.baseTime;
+  });
+  callHeatListeners();
 }
 
 export {
@@ -60,6 +85,7 @@ export {
   defaultState,
   currentState,
   resetState,
+  attachHeatListener,
+  detachHeatListener,
+  increaseHeat,
 };
-
-// WE PROBABLY WILL ADD MORE STUFF HERE LATER
